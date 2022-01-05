6e30a96da88e0ea9eec2adf706c30a28d5ec6bfc
Rebeka Odilon <49457748+rebekaodilon@users.noreply.github.com>
2022-01-05 16:07:45 -0300

    atualização ftp

--- index.php
+++ index.php
@@ -1,51 +1,68 @@
 <?php
 
+require_once '../zk_api/src/services/ftp/FtpClient.php';
+require_once '../zk_api/src/services/ftp/FtpException.php';
+require_once '../zk_api/src/services/ftp/FtpWrapper.php';
+
+// $ftp = new \FtpClient\FtpClient();
+// $ftp->connect('ftp.zukk.in');
+// $ftp->login('testes_zukkin', 'yC8peZbUuUdXQKuB');
+// $ftp->pasv(true);
+
+// $file = $ftp->getContent("/arquivos/arquivo1.txt");
+// var_dump($file); die;
+
+
 $ftp_server = "ftp.zukk.in";
 $ftp_conn = ftp_connect($ftp_server);
 $login = ftp_login($ftp_conn, 'testes_zukkin', 'yC8peZbUuUdXQKuB');
+ftp_pasv($ftp_conn, true);
 
-$file_list = ftp_nlist($ftp_conn, ".");
-var_dump($file_list);
+$file = ftp_nlist($ftp_conn, "/arquivos");
+var_dump($file); die;
+
+for ($i=0; $i < count($listaArquivos); $i++) {
+
+    $nomeArquivo = $listaArquivos[$i];
+    $separadorLinha = ";\r\n";
+
+    if (file_exists($nomeArquivo)) 
+    {
+        $tamanhoArquivo = filesize($nomeArquivo);
+        $arquivo = fopen($nomeArquivo, 'r+');
+        $totalLinhasArquivo = count(file($nomeArquivo));
+        $contaLinhas = 0;
+        $maxLinhas = 1000;
+        $resultado = array();
+        $cont = 0;
+
+        while(!feof($arquivo))
+        {
+            if ($contaLinhas == $maxLinhas)
+            {
+                unset($resultado);
+                $resultado = array();
+                $contaLinhas = 0;
+            }
+            else if ($contaLinhas < $maxLinhas)
+            {
+                $resultado[] = explode($separadorLinha, fgets($arquivo, $tamanhoArquivo));
+                // Faz o processamento do array
+            }
+            else
+            {
+                break;
+            }
+            $contaLinhas++;
+            $cont++;
+        }
+        echo json_encode($resultado);
+        fclose($arquivo);
+    } 
+    else 
+    {
+        exit('ERRO => Arquivo vazio ou inexistente.');
+    }
+}
 
 ftp_close($ftp_conn);
-
-$nomeArquivo = 'arquivos/arquivo1.txt';
-$separadorLinha = ";\r\n";
-
-if (file_exists($nomeArquivo)) 
-{
-    $tamanhoArquivo = filesize($nomeArquivo);
-    $arquivo = fopen($nomeArquivo, 'r+');
-    $totalLinhasArquivo = count(file($nomeArquivo));
-    $contaLinhas = 0;
-    $maxLinhas = 1000;
-    $resultado = array();
-    $cont = 0;
-
-    while(!feof($arquivo))
-    {
-        if ($contaLinhas == $maxLinhas)
-        {
-            unset($resultado);
-            $resultado = array();
-            $contaLinhas = 0;
-        }
-        else if ($contaLinhas < $maxLinhas)
-        {
-            $resultado[] = explode($separadorLinha, fgets($arquivo, $tamanhoArquivo));
-            // Faz o processamento do array
-        }
-        else
-        {
-            break;
-        }
-        $contaLinhas++;
-        $cont++;
-    }
-    // echo json_encode($resultado);
-    fclose($arquivo);
-} 
-else 
-{
-    exit('ERRO => Arquivo vazio ou inexistente.');
-}
